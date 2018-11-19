15c9a7afd1fb1b07ca1aa7d78e02a970af518a9f
Denis Butuzov <qwaswedo@gmail.com>
2018-11-19 18:32:57 +0700

    Added code for showing scene

--- main.cpp
+++ main.cpp
@@ -1,9 +1,27 @@
 #include <QApplication>
 #include <QGraphicsScene>
+#include <QGraphicsRectItem>
+#include <QGraphicsView>
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
+
+    // create a scene
+    QGraphicsScene *scene = new QGraphicsScene();
+
+    //create an item to put unto the scene
+    QGraphicsRectItem *rect = new QGraphicsRectItem();
+    rect->setRect(0, 0, 100, 100);
+
+    //add the item to the scene
+    scene->addItem(rect);
+
+    //add a view
+    QGraphicsView *view = new QGraphicsView();
+    view->setScene(scene);
+
+    view->show();
 
     return a.exec();
 }
