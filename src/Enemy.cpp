3c94f72f8ef9e8665868686627f4d39908361b2e
Denis Butuzov <qwaswedo@gmail.com>
2018-11-20 21:54:32 +0700

    Added destroing player and enemy

--- src/Enemy.cpp
+++ src/Enemy.cpp
@@ -27,6 +27,24 @@ Enemy::Enemy()
 
 void Enemy::move()
 {
+    //destroy player and enemy
+    QList<QGraphicsItem *> colliding_items = collidingItems();
+    for(int i = 0, n = colliding_items.size(); i < n; i++)
+    {
+        if(typeid(*(colliding_items[i])) == typeid(Player))
+        {
+            //remove them both from scene
+            scene()->removeItem(colliding_items[i]);
+            scene()->removeItem(this);
+
+            //delete them both
+            delete colliding_items[i];
+            delete this;
+
+            return;
+        }
+    }
+
     //move enemy down
     setPos(x(), y() + 5);
 
