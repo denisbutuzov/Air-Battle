95343708779e832d75b5da440a2f3db24a4fa7ca
Denis Butuzov <qwaswedo@gmail.com>
2018-11-20 21:25:30 +0700

    Added alternation shots

--- src/Player.cpp
+++ src/Player.cpp
@@ -8,22 +8,37 @@
 
 void Player::keyPressEvent(QKeyEvent *event)
 {
+    //variable for alternation shots
+    static int sideCounter = 0;
+
+    //press left key - move to left
     if(event->key() == Qt::Key_Left)
     {
         if(x() > 0)
+        {
             setPos(x() - 10, y());
+        }
     }
+    //press right key - move to right
     else if(event->key() == Qt::Key_Right)
     {
         if(x() + 100 < 800)
+        {
             setPos(x() + 10, y());
+        }
     }
-    //create a bullet
+    //press space key - create a bullet
     else if(event->key() == Qt::Key_Space)
     {
         Bullet *bullet = new Bullet();
-        bullet->setPos(x(), y());
+        bullet->setPos(x() + sideCounter * 90, y());
         scene()->addItem(bullet);
+
+        sideCounter++;
+        if(sideCounter > 1)
+        {
+            sideCounter = 0;
+        }
     }
 }
 
