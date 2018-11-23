b759dbccec41f1941fea91d6eeebebf57c61e52b
Denis Butuzov <qwaswedo@gmail.com>
2018-11-23 21:07:12 +0700

    Added some code for Weapon class

--- include/weapons/Weapon.h
+++ include/weapons/Weapon.h
@@ -1,1 +1,14 @@
+#pragma once
+
+#include <QGraphicsPixmapItem>
+
+#include "GunShell.h"
+
+class Weapon: public QGraphicsPixmapItem
+{
+private:
+    GunShell *gunShell_;
+public slots:
+    void move();
+};
 
---
+++ src/weapons/Weapon.cpp
@@ -0,0 +1,32 @@
+#include <QGraphicsScene>
+
+#include "Player.h"
+
+#include "Weapon.h"
+
+void Weapon::move()
+{
+    //destroy weapon
+    QList<QGraphicsItem *> colliding_items = collidingItems();
+    for(int i = 0, n = colliding_items.size(); i < n; i++)
+    {
+        if(typeid(*(colliding_items[i])) == typeid(Player))
+        {
+            scene()->removeItem(this);
+            delete this;
+
+            return;
+        }
+    }
+
+    //move weapon down
+    setPos(x(), y() + 5);
+
+    //delete weapon
+    if(y() > scene()->height())
+    {
+        //remove from the scene
+        scene()->removeItem(this);
+        delete this;
+    }
+}
