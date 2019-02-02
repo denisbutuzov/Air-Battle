921e4ad5b64e925f64fac9062bac47f8c3760c9e
Denis Butuzov <qwaswedo@gmail.com>
2019-02-02 17:43:32 +0700

    Rewrite Health-class to singleton

--- include/Health.h
+++ include/Health.h
@@ -9,18 +9,25 @@ const int MAX_HEARTS_NUMBER = 5;
 class Health
 {
 public:
-    Health(QGraphicsScene *scene, QPointF coordinate);
-    ~Health() = default;
-
+    static Health *instance();
+    void show(QGraphicsScene *scene, QPointF coordinate = QPointF(0.0, 0.0));
     void increase();
     void decrease();
     QPointF coordinate() const;
     const QQueue<HeartObject *> &hearts() const;
 
+protected:
+    Health();
+    Health(const Health &other) = delete;
+    Health(Health &&other) = delete;
+
+    Health &operator=(const Health &other) = delete;
+
 private:
     void addHeart();
 
 private:
+    static Health *instance_;
     QQueue<HeartObject *> hearts_;
     const int maxHeartsNumber_;
     QGraphicsScene *scene_;
