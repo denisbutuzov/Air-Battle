40230e237cec549652532b9efdd51cfa3e86dab5
Denis Butuzov <qwaswedo@gmail.com>
2019-02-19 19:31:01 +0700

    Replace direction() method from protected to private section

--- include/gameobjects/movestrategies/MoveStrategy.h
+++ include/gameobjects/movestrategies/MoveStrategy.h
@@ -14,10 +14,10 @@ public:
     virtual ~MoveStrategy() = default;
     virtual void move(qreal &x, qreal &y, qreal speed) = 0;
 
+    DIRECTION direction() const;
+
 protected:
     MoveStrategy(DIRECTION dir);
-
-    DIRECTION direction() const;
 
 private:
     DIRECTION dir_;
