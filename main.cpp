2230c1e0fef3908b7365aa936253697bc6d262e9
Denis Butuzov <qwaswedo@gmail.com>
2019-02-21 21:05:06 +0700

    Added srand() function in main.cpp

--- main.cpp
+++ main.cpp
@@ -1,11 +1,14 @@
 #include <QApplication>
 
+#include <ctime>
+
 #include "Game.h"
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
 
+    std::srand(static_cast<unsigned int>(std::time(nullptr)));
     Game *game = new Game();
     game->show();
 
