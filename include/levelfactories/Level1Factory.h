c79c9960356071cec0d81feca7751e46118c2f5a
Denis Butuzov <qwaswedo@gmail.com>
2019-02-23 23:25:50 +0700

    Fixed code style in Level1Factory-class

--- include/levelfactories/Level1Factory.h
+++ include/levelfactories/Level1Factory.h
@@ -8,7 +8,6 @@ class Level1Factory
 public:
     Level1Factory(QGraphicsScene *scene);
     virtual ~Level1Factory() override = default;
-
     virtual Enemy *enemy() const override final;
     virtual Weapon *weapon() const override final;
 };
