10aa23249f765dd60838b8f8e48b7206520ab954
Denis Butuzov <qwaswedo@gmail.com>
2019-07-13 19:53:07 +0700

    Added destroying image of gunshell

--- src/GameObjects/Gunshells/Gunshell.cpp
+++ src/GameObjects/Gunshells/Gunshell.cpp
@@ -1,6 +1,11 @@
+#include <QGraphicsScene>
+#include <QTimer>
+
 #include "Visitors/AbstractVisitor.h"
 
 #include "Gunshell.h"
+
+constexpr const char *GUNSHELL_BOOM_IMAGE = ":/images/images/Gunshell_boom.png";
 
 Gunshell::Gunshell(std::weak_ptr<QGraphicsScene> scene,
                    int damage,
@@ -8,6 +13,20 @@ Gunshell::Gunshell(std::weak_ptr<QGraphicsScene> scene,
     : MovableObject(scene, moveStrategy)
     , damage_(damage)
 {
+}
+
+Gunshell::~Gunshell()
+{
+    if(auto wp = scene().lock())
+    {
+        if(y() > 0)
+        {
+            QGraphicsPixmapItem *pixmap = new QGraphicsPixmapItem(QPixmap(GUNSHELL_BOOM_IMAGE));
+            pixmap->setPos(pos());
+            wp->addItem(pixmap);
+            QTimer::singleShot(std::chrono::milliseconds(100), [pixmap = pixmap](){ delete pixmap; });
+        }
+    }
 }
 
 void Gunshell::accept(AbstractVisitor &visitor)
