909ae8fe4c54c206ae606bcac897d545944c8182
Denis Butuzov <qwaswedo@gmail.com>
2019-07-21 16:17:45 +0700

    Added documentation for MovableObject-class

--- src/GameObjects/MovableObject.cpp
+++ src/GameObjects/MovableObject.cpp
@@ -4,6 +4,10 @@
 
 #include "MovableObject.h"
 
+/*!
+ * \param scene Слабый указатель на объект сцены.
+ * \param moveStrategy Указатель на разделяемый объект стратегии движения.
+ */
 MovableObject::MovableObject(std::weak_ptr<QGraphicsScene> scene,
                              std::shared_ptr<MoveStrategy> moveStrategy)
     : GameObject(scene)
@@ -11,21 +15,37 @@ MovableObject::MovableObject(std::weak_ptr<QGraphicsScene> scene,
 {
 }
 
+/*!
+ * Делегирует обязанности передвижения объекта стратегии движения,
+ * в котором реализован соответствующий алгоритм движения.
+ */
 void MovableObject::move()
 {
     moveStrategy_->move(*this);
 }
 
+/*!
+ * \param speed Скорость движения объекта.
+ *
+ * Устанавливает скорость передвижения объекта путем установки скорости
+ * движения объекту стратегии движения.
+ */
 void MovableObject::setSpeed(unsigned int speed)
 {
     moveStrategy_->setSpeed(speed);
 }
 
+/*!
+ * \return Скорость движения объекта.
+ */
 unsigned int MovableObject::speed() const
 {
     return moveStrategy_->speed();
 }
 
+/*!
+ * \return Указатель на разделяемый объект статегии движения.
+ */
 std::shared_ptr<MoveStrategy> MovableObject::moveStrategy() const
 {
     return moveStrategy_;
--- src/GameObjects/MovableObject.h
+++ src/GameObjects/MovableObject.h
@@ -5,6 +5,11 @@
 class AbstractVisitor;
 class MoveStrategy;
 
+/*!
+ * \brief Родительский класс всех способных к движению объектов сцены (кроме PlayerObject).
+ *
+ * Реализует механизм движения игровых объектов на сцене.
+ */
 class MovableObject
         : public QObject
         , public GameObject
@@ -12,13 +17,20 @@ class MovableObject
     Q_OBJECT
 
 public:
+    ///Деструктор по умолчанию.
     virtual ~MovableObject() override = default;
+    ///Метод, принимающий объект посетителя.
     virtual void accept(AbstractVisitor &visitor) = 0;
+    ///Метод перемещения объекта.
     virtual void move();
+    ///Метод для установки скорости объекта.
     void setSpeed(unsigned int speed);
+    ///Метод, возвращающий скорость объекта.
     unsigned int speed() const;
+    ///Метод, возвращающий указатель на разделяемый объект стратегии движения.
     std::shared_ptr<MoveStrategy> moveStrategy() const;
 protected:
+    ///Конструктор с двумя аргументами.
     MovableObject(std::weak_ptr<QGraphicsScene> scene,
                   std::shared_ptr<MoveStrategy> moveStrategy);
 private:
