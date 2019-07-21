b2f5e1044d22bbd1d61157ca8e89b138a7cdc028
Denis Butuzov <qwaswedo@gmail.com>
2019-07-21 16:17:45 +0700

    Added documentation for MovableObject-class

--- src/GameObjects/MovableObject.h
+++ src/GameObjects/MovableObject.h
@@ -5,6 +5,13 @@
 class AbstractVisitor;
 class MoveStrategy;
 
+/*!
+ * \ingroup Game_objects
+ * \brief Абстрактный родительский класс всех способных к движению
+ * объектов сцены (кроме PlayerObject).
+ *
+ * Реализует механизм движения игровых объектов на сцене.
+ */
 class MovableObject
         : public QObject
         , public GameObject
@@ -12,13 +19,20 @@ class MovableObject
     Q_OBJECT
 
 public:
+    ///Деструктор по умолчанию.
     virtual ~MovableObject() override = default;
+    ///Метод, принимающий объект посетителя.
     virtual void accept(AbstractVisitor &visitor) = 0;
+    ///Метод перемещения объекта.
     virtual void move();
+    ///Метод для установки скорости объекта.
     void setSpeed(unsigned int speed);
+    ///Метод, возвращающий скорость объекта.
     unsigned int speed() const;
+    ///Метод, возвращающий указатель на разделяемый объект стратегии движения.
     std::shared_ptr<MoveStrategy> moveStrategy() const;
 protected:
+    ///Конструктор с двумя аргументами.
     MovableObject(std::weak_ptr<QGraphicsScene> scene,
                   std::shared_ptr<MoveStrategy> moveStrategy);
 private:
