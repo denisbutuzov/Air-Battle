c21c3107cc37be559a0d148fb186508b980c8c5d
Denis Butuzov <qwaswedo@gmail.com>
2019-07-22 21:01:40 +0700

    Added documentation for Bazooka-class

--- src/GameObjects/Weapons/Bazooka.cpp
+++ src/GameObjects/Weapons/Bazooka.cpp
@@ -2,12 +2,19 @@
 
 #include "Bazooka.h"
 
+/*!
+ * \param scene Слабый указатель на объект сцены.
+ * \param moveStrategy Указатель на разделяемый объект стратегии движения.
+ */
 Bazooka::Bazooka(std::weak_ptr<QGraphicsScene> scene,
                  std::shared_ptr<MoveStrategy> moveStrategy)
     : Weapon(scene, moveStrategy)
 {
 }
 
+/*!
+ * \return Указатель с исключительными правами владения объектом ручной базуки.
+ */
 std::unique_ptr<HandWeapon> Bazooka::handWeapon()
 {
     return std::make_unique<HandBazooka>(scene());
--- src/GameObjects/Weapons/Bazooka.h
+++ src/GameObjects/Weapons/Bazooka.h
@@ -3,12 +3,21 @@
 #include "MoveStrategies/MoveByLine.h"
 #include "Weapon.h"
 
+/*!
+ * \ingroup Game_objects
+ * \brief Класс базуки.
+ *
+ * Реализует класс базуки, которую может подобрать игрок.
+ */
 class Bazooka
         : public Weapon
 {
 public:
+    ///Конструктор с двумя аргументами.
     Bazooka(std::weak_ptr<QGraphicsScene> scene,
             std::shared_ptr<MoveStrategy> moveStrategy = std::make_shared<MoveByLine>(MoveStrategy::Direction::Down, 6));
+    ///Деструктор по умолчанию.
     virtual ~Bazooka() override = default;
+    ///Метод, пораждающий объект ручной базуки.
     virtual std::unique_ptr<HandWeapon> handWeapon() override final;
 };
