cdb90cd1f3d54cc68f0da41d6c5b89bbcafb8dd0
Denis Butuzov <qwaswedo@gmail.com>
2019-07-22 20:58:47 +0700

    Added documentation for Machinegun-class

--- src/GameObjects/Weapons/Machinegun.cpp
+++ src/GameObjects/Weapons/Machinegun.cpp
@@ -2,12 +2,19 @@
 
 #include "Machinegun.h"
 
+/*!
+ * \param scene Слабый указатель на объект сцены.
+ * \param moveStrategy Указатель на разделяемый объект стратегии движения.
+ */
 Machinegun::Machinegun(std::weak_ptr<QGraphicsScene> scene,
                        std::shared_ptr<MoveStrategy> moveStrategy)
     : Weapon(scene, moveStrategy)
 {
 }
 
+/*!
+ * \return Указатель с исключительными правами владения объектом ручного пулемета.
+ */
 std::unique_ptr<HandWeapon> Machinegun::handWeapon()
 {
     return std::make_unique<HandMachinegun>(scene());
