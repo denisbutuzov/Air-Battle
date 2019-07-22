2da0c12ae9aa970c78c942f844809c857aa6193d
Denis Butuzov <qwaswedo@gmail.com>
2019-07-22 22:16:01 +0700

    Added documentation for AbstractEnemyDecorator-class

--- src/GameObjects/Enemies/EnemyDecorators/AbstractEnemyDecorator.h
+++ src/GameObjects/Enemies/EnemyDecorators/AbstractEnemyDecorator.h
@@ -2,14 +2,24 @@
 
 #include "GameObjects/Enemies/Enemy.h"
 
+/*!
+ * \ingroup Game_objects
+ * \brief Абстрактный родительский класс для всех декораторов врага.
+ *
+ * Реализует класс декораторов для врага.
+ */
 class AbstractEnemyDecorator
         : public Enemy
 {
 public:
+    ///Деструктор по умолчанию.
     virtual ~AbstractEnemyDecorator() override = default;
+    ///Метод инициализации врага и декоратора на сцене.
     virtual void init() override;
+    ///Метод, возвращающий декорируемый объект врага.
     std::unique_ptr<Enemy> &enemy();
 protected:
+    ///Конструктор с двумя аргументами.
     AbstractEnemyDecorator(std::unique_ptr<Enemy> enemy, int hitpoint);
 private:
     std::unique_ptr<Enemy> enemy_;
