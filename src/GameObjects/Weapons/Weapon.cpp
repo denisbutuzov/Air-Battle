a2545cd0bbe673d541218b3b000ff5ce77c6f56b
Denis Butuzov <qwaswedo@gmail.com>
2019-07-22 20:50:59 +0700

    Added documentation for Weapon-class

--- src/GameObjects/Weapons/Weapon.cpp
+++ src/GameObjects/Weapons/Weapon.cpp
@@ -2,12 +2,22 @@
 
 #include "Weapon.h"
 
+/*!
+ * \param scene Слабый указатель на объект сцены.
+ * \param moveStrategy Указатель на разделяемый объект стратегии движения.
+ */
 Weapon::Weapon(std::weak_ptr<QGraphicsScene> scene,
                std::shared_ptr<MoveStrategy> moveStrategy)
     : MovableObject(scene, moveStrategy)
 {
 }
 
+/*!
+ * \param visitor Объект посетителя.
+ *
+ * Вызывает метод `void visitWeapon(Weapon *weapon)` переданного в качестве параметра
+ * объекта посетителя.
+ */
 void Weapon::accept(AbstractVisitor &visitor)
 {
     visitor.visitWeapon(this);
--- src/GameObjects/Weapons/Weapon.h
+++ src/GameObjects/Weapons/Weapon.h
@@ -4,14 +4,24 @@
 
 class HandWeapon;
 
+/*!
+ * \ingroup Game_objects
+ * \brief Абстрактный родительский класс всех класов оружия.
+ *
+ * Реализует класс оружия, которое может подобрать игрок.
+ */
 class Weapon
         : public MovableObject
 {
 public:
+    ///Деструктор по умолчанию.
     virtual ~Weapon() override = default;
+    ///Метод, принимающий объект посетителя.
     virtual void accept(AbstractVisitor &visitor) override final;
+    ///Метод, порождающий соответствующий объект ручного оружия.
     virtual std::unique_ptr<HandWeapon> handWeapon() = 0;
 protected:
+    ///Конструктор с двумя аргументами.
     Weapon(std::weak_ptr<QGraphicsScene> scene,
            std::shared_ptr<MoveStrategy> moveStrategy);
 };
