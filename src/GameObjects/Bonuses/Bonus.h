cab6af5d11423ba93304b7acbb7b7f1d681105bd
Denis Butuzov <qwaswedo@gmail.com>
2019-07-22 21:13:01 +0700

    Added documentation for Bonus-class

--- src/GameObjects/Bonuses/Bonus.h
+++ src/GameObjects/Bonuses/Bonus.h
@@ -6,14 +6,28 @@ class HandBonus;
 class PlayerObject;
 class GameObjectStorage;
 
+/*!
+ * \ingroup Game_objects
+ * \brief Абстрактный родительский класс всех игровых бонусов.
+ *
+ * Реализует класс бонуса, который может подобрать игрок.
+ *
+ * \todo
+ * - класс бонуса, замораживающего врагов на игровой сцене;
+ * - класс бонуса, отталкивающегшо мощным импульсом врагов на несколько позиций назад.
+ */
 class Bonus
         : public MovableObject
 {
 public:
+    ///Деструктор по умолчанию.
     virtual ~Bonus() override = default;
+    ///Метод, принимающий объект посетителя.
     virtual void accept(AbstractVisitor &visitor) override final;
+    ///Метод, порождающий соответствующий объект ручного бонуса.
     virtual std::unique_ptr<HandBonus> handBonus(PlayerObject *player, GameObjectStorage *storage) = 0;
 protected:
+    ///Конструктор с двумя аргументами.
     Bonus(std::weak_ptr<QGraphicsScene> scene,
            std::shared_ptr<MoveStrategy> moveStrategy);
 };
