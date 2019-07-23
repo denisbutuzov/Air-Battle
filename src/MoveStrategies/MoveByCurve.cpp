5755157edbc16b97b457ae2adc2eac7a9b4c211a
Denis Butuzov <qwaswedo@gmail.com>
2019-07-23 23:42:02 +0700

    Added documentation for MoveByCurve-class

--- src/MoveStrategies/MoveByCurve.cpp
+++ src/MoveStrategies/MoveByCurve.cpp
@@ -4,12 +4,22 @@
 
 #include "MoveByCurve.h"
 
+/*!
+ * \param dir Направление движения объекта.
+ * \param speed Скорость движения объекта.
+ */
 MoveByCurve::MoveByCurve(MoveStrategy::Direction dir,
                          unsigned int speed)
     : MoveStrategy(dir, speed)
 {
 }
 
+/*!
+ * \param object Игровой объекта для перемещения.
+ *
+ * Перемещает игровой объект по кривой синусоидальной линии
+ * **вверх** или **вниз** в зависимости от направления движения.
+ */
 void MoveByCurve::move(MovableObject &object)
 {
     if(direction() == Direction::Up)
--- src/MoveStrategies/MoveByCurve.h
+++ src/MoveStrategies/MoveByCurve.h
@@ -2,10 +2,19 @@
 
 #include "MoveStrategy.h"
 
+/*!
+ * \ingroup Move_strategies
+ * \brief Класс стратегии движени по кривой линии.
+ *
+ * Реализует стратегию движения по кривой синусоидальной линии
+ * **вверх** или **вниз** в зависимости от направления движения.
+ */
 class MoveByCurve
         : public MoveStrategy
 {
 public:
+    ///Конструктор с двумя аргументами.
     MoveByCurve(Direction dir, unsigned int);
+    ///Метод перемещения игрового объекта.
     virtual void move(MovableObject &object) override final;
 };
