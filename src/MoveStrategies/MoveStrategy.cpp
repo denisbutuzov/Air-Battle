f1d1dd547ec32350191e08fd6c6cb7199c05e673
Denis Butuzov <qwaswedo@gmail.com>
2019-07-23 23:24:41 +0700

    Added documentation for MoveStrategy-class

--- src/MoveStrategies/MoveStrategy.cpp
+++ src/MoveStrategies/MoveStrategy.cpp
@@ -1,5 +1,9 @@
 #include "MoveStrategy.h"
 
+/*!
+ * \param dir Направление движения объекта.
+ * \param speed Скорость движения объекта.
+ */
 MoveStrategy::MoveStrategy(MoveStrategy::Direction dir,
                            unsigned int speed)
     : dir_(dir)
@@ -7,16 +11,27 @@ MoveStrategy::MoveStrategy(MoveStrategy::Direction dir,
 {
 }
 
+/*!
+ * \return Направление движения объекта.
+ */
 MoveStrategy::Direction MoveStrategy::direction() const
 {
     return dir_;
 }
 
+/*!
+ * \param speed Скорость игрового объекта.
+ *
+ * Устанавливает скорость передвижения игрового объекта.
+ */
 void MoveStrategy::setSpeed(unsigned int speed)
 {
     speed_ = speed;
 }
 
+/*!
+ * \return Скорость движения объекта.
+ */
 unsigned int MoveStrategy::speed() const
 {
     return speed_;
--- src/MoveStrategies/MoveStrategy.h
+++ src/MoveStrategies/MoveStrategy.h
@@ -2,21 +2,46 @@
 
 class MovableObject;
 
+/*!
+ * \defgroup Move_strategies Стратегии движения объектов
+ * \brief Данный модуль описывает различные стратегии движения игровых объектов.
+ */
+
+/*!
+ * \ingroup Move_strategies
+ * \brief Абстрактный родительский класс стратегий движения
+ * игровых объектов.
+ *
+ * Реализует основные методы стратегии движения игровых
+ * объектов по игровой сцене.
+ */
 class MoveStrategy
 {
 public:
+    /*!
+     * \brief Направление движения.
+     *
+     * Используется для конкретизации направления движения
+     * игрового объекта.
+     */
     enum class Direction
     {
-        Up,
-        Down
+        Up, ///<Движение вверх
+        Down ///<Движение вниз
     };
 public:
+    ///Деструктор по умолчанию.
     virtual ~MoveStrategy() = default;
+    ///Метод перемещения игрового объекта.
     virtual void move(MovableObject &object) = 0;
+    ///Метод, возвращающий направление движения игрового объекта.
     Direction direction() const;
+    ///Метод для установки скорости объекта.
     void setSpeed(unsigned int speed);
+    ///Метод, возвращающий скорость объекта.
     unsigned int speed() const;
 protected:
+    ///Конструктор с двумя аргументами.
     MoveStrategy(Direction dir, unsigned int speed);
 private:
     Direction dir_;
