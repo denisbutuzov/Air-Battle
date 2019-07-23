9d7607d9fad0f564947510892cbb6bb585c17ab7
Denis Butuzov <qwaswedo@gmail.com>
2019-07-23 23:34:18 +0700

    Added documentation for MoveByLine-class

--- src/MoveStrategies/MoveByLine.cpp
+++ src/MoveStrategies/MoveByLine.cpp
@@ -2,12 +2,22 @@
 
 #include "MoveByLine.h"
 
+/*!
+ * \param dir Направление движения объекта.
+ * \param speed Скорость движения объекта.
+ */
 MoveByLine::MoveByLine(MoveStrategy::Direction dir,
                        unsigned int speed)
     : MoveStrategy(dir, speed)
 {
 }
 
+/*!
+ * \param object Игровой объекта для перемещения.
+ *
+ * Перемещает игровой объект вдоль прямой линии **вверх** или
+ * **вниз** в зависимости от направления движения.
+ */
 void MoveByLine::move(MovableObject &object)
 {
     if(direction() == Direction::Up)
--- src/MoveStrategies/MoveByLine.h
+++ src/MoveStrategies/MoveByLine.h
@@ -2,10 +2,19 @@
 
 #include "MoveStrategy.h"
 
+/*!
+ * \ingroup Move_strategies
+ * \brief Класс стратегии движения вдоль прямой линии.
+ *
+ * Реализует стратегию движения вдоль прямой линии **вверх** или
+ * **вниз** в зависимости от направления движения.
+ */
 class MoveByLine
         : public MoveStrategy
 {
 public:
-    MoveByLine(Direction dir, unsigned int speed);
+    ///Конструктор с двумя аргументами.
+    MoveByLine(Direction dir, unsigned int speed);\
+    ///Метод перемещения игрового объекта.
     virtual void move(MovableObject &object) override final;
 };
