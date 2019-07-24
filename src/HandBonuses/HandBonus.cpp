15cc9da69f41fd5d7f3e0385a57dd521d40597c1
Denis Butuzov <qwaswedo@gmail.com>
2019-07-24 20:28:37 +0700

    Added documentation for HandBonus-class

--- src/HandBonuses/HandBonus.cpp
+++ src/HandBonuses/HandBonus.cpp
@@ -3,17 +3,27 @@
 
 #include "HandBonus.h"
 
+/*!
+ * \param player Указатель на объект, управляемый игроком.
+ * \param storage Указатель на объект хранилища игровых объектов сцены.
+ */
 HandBonus::HandBonus(PlayerObject *player, GameObjectStorage *storage)
     : player_(player)
     , storage_(storage)
 {
 }
 
+/*!
+ * \return Указатель на объект, управляемый игроком.
+ */
 PlayerObject *HandBonus::player() const
 {
     return player_;
 }
 
+/*!
+ * \return Указатель на объект хранилища игровых объектов.
+ */
 GameObjectStorage *HandBonus::storage() const
 {
     return storage_;
--- src/HandBonuses/HandBonus.h
+++ src/HandBonuses/HandBonus.h
@@ -3,14 +3,31 @@
 class PlayerObject;
 class GameObjectStorage;
 
+/*!
+ * \defgroup Hand_bonuses Ручные используемые бонусы
+ * \brief Данный модуль описывает все ручные бонусы, которые может
+ * использовать игрок.
+ */
+
+/*!
+ * \ingroup Hand_bonuses
+ * \brief Абстрактный родительский класс всех ручных используемых бонусов.
+ *
+ * Реализует класс ручного бонуса, который может использовать игрок.
+ */
 class HandBonus
 {
 public:
+    ///Конструктор с двумя аргументами.
     HandBonus(PlayerObject *player, GameObjectStorage *storage);
+    ///Деструктор по умолчанию.
     virtual ~HandBonus() = default;
+    ///Метод использования бонуса.
     virtual void execute() = 0;
 protected:
+    ///Метод, возвращающий указатель на объект, управляемый игроком.
     PlayerObject *player() const;
+    ///Метод, возвращающий указатель на объект хранилища игровых объектов.
     GameObjectStorage *storage() const;
 private:
     PlayerObject *player_;
