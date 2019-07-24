4b3d3ad241d3953b18061d4fe2775717f9cc5174
Denis Butuzov <qwaswedo@gmail.com>
2019-07-24 20:54:35 +0700

    Added documentation for AbstractSubject-class

--- src/SpecialObjects/Subjects/AbstractSubject.cpp
+++ src/SpecialObjects/Subjects/AbstractSubject.cpp
@@ -2,16 +2,32 @@
 
 #include "AbstractSubject.h"
 
+/*!
+ * \param observer Прикрепляемый наблюдатель.
+ *
+ * Добавляет переданного наблюдателя в список наблюдателей, отслеживающих
+ * состояние данного субъекта.
+ */
 void AbstractSubject::attach(AbstractObserver *observer)
 {
     observers_.push_back(observer);
 }
 
+/*!
+ * \param observer Отвязываемый наблюдатель.
+ *
+ * Исключает переданного наблюдателя из списка наблюдателей, отслеживающих
+ * состояние данного субъекта.
+ */
 void AbstractSubject::detach(AbstractObserver *observer)
 {
     observers_.remove(observer);
 }
 
+/*!
+ * Оповещает всех наблюдателей об изменении, произошедшем в состоянии
+ * данного субъекта.
+ */
 void AbstractSubject::notify()
 {
     for(auto *observer : observers_)
--- src/SpecialObjects/Subjects/AbstractSubject.h
+++ src/SpecialObjects/Subjects/AbstractSubject.h
@@ -4,14 +4,33 @@
 
 class AbstractObserver;
 
+/*!
+ * \defgroup Subjects Субъекты для наблюдения
+ * \brief Данный модуль описывает все субъекты для наблюдения.
+ */
+
+/*!
+ * \ingroup Subjects
+ * \brief Абстрактный родительский класс всех субъектов для наблюдения.
+ *
+ * Реализует интерфейс субъекта для наблюдения:
+ * - присоединение наблюдателя;
+ * - отвязывание наблюдателя;
+ * - оповещение наблюдателей.
+ */
 class AbstractSubject
 {
 public:
+    ///Деструктор по умолчанию.
     virtual ~AbstractSubject() = default;
+    ///Метод присоединения наблюдателя.
     virtual void attach(AbstractObserver *observer);
+    ///Метод отвязывания наблюдателя.
     virtual void detach(AbstractObserver *observer);
+    ///Метод оповещения наблюдателей.
     virtual void notify();
 protected:
+    ///Конструкто по умолчанию.
     AbstractSubject() = default;
 private:
     std::list<AbstractObserver *> observers_;
