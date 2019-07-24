74e32b5b159d8b43a2002ab998cafa7a5f79ae23
Denis Butuzov <qwaswedo@gmail.com>
2019-07-24 21:06:33 +0700

    Added documentation for Score-class

--- src/SpecialObjects/Subjects/Score.cpp
+++ src/SpecialObjects/Subjects/Score.cpp
@@ -1,15 +1,26 @@
 #include "Score.h"
 
+/*!
+ * Инициализирует значение счета в игре нулем.
+ */
 Score::Score()
     : score_(0)
 {
 }
+
+/*!
+ * Увеличивает значение счета в игре на единицу и оповещает
+ * всех наблюдателей.
+ */
 void Score::increase()
 {
     ++score_;
     notify();
 }
 
+/*!
+ * \return Значение счета в игре.
+ */
 unsigned int Score::value() const
 {
     return score_;
