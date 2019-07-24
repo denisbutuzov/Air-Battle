26dfd8f9f537d123c37a352f7428ff804bc92fdc
Denis Butuzov <qwaswedo@gmail.com>
2019-07-24 20:37:25 +0700

    Added documentation for HandDetonation-class

--- src/HandBonuses/HandDetonation.cpp
+++ src/HandBonuses/HandDetonation.cpp
@@ -3,11 +3,18 @@
 
 #include "HandDetonation.h"
 
+/*!
+ * \param player Указатель на объект, управляемый игроком.
+ * \param storage Указатель на объект хранилища игровых объектов сцены.
+ */
 HandDetonation::HandDetonation(PlayerObject *player, GameObjectStorage *storage)
     : HandBonus(player, storage)
 {
 }
 
+/*!
+ * Реализует уничтожение всех находящихся на сцене врагов.
+ */
 void HandDetonation::execute()
 {
     storage()->enemies().clear();
--- src/HandBonuses/HandDetonation.h
+++ src/HandBonuses/HandDetonation.h
@@ -2,11 +2,21 @@
 
 #include "HandBonus.h"
 
+/*!
+ * \ingroup Hand_bonuses
+ * \brief Класс ручного взрывающего бонуса.
+ *
+ * Реализует класс ручного взрываюещего при использовании
+ * всех находящихся на игровой сцене врагов.
+ */
 class HandDetonation
         : public HandBonus
 {
 public:
+    ///Конструктор с двумя аргументами.
     HandDetonation(PlayerObject *player, GameObjectStorage *storage);
+    ///Деструктор по умолчанию.
     virtual ~HandDetonation() override = default;
+    ///Метод использования взрывающего бонуса.
     virtual void execute() override;
 };
