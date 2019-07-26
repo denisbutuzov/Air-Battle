c306781b5b8c8c4bfbfb357a490c0ff551d24fbf
Denis Butuzov <qwaswedo@gmail.com>
2019-07-26 22:40:52 +0700

    Added documentation for HandBazooka-class

--- src/HandWeapons/HandBazooka.cpp
+++ src/HandWeapons/HandBazooka.cpp
@@ -3,6 +3,9 @@
 
 #include "HandBazooka.h"
 
+/*!
+ * \param scene Слабый указатель на объект сцены.
+ */
 HandBazooka::HandBazooka(std::weak_ptr<QGraphicsScene> scene)
     : HandWeapon(scene, AppSettings::instance().objects().bazooka_.capacity_,
                  AppSettings::instance().objects().bazooka_.startPatrons_,
@@ -11,6 +14,16 @@ HandBazooka::HandBazooka(std::weak_ptr<QGraphicsScene> scene)
 {
 }
 
+/*!
+ * \param x Координата по оси Х на игровой сцене для порождения
+ * оружейного снаряда базуки.
+ * \param y Координата по оси Y на игровой сцене для порождения
+ * оружейного снаряда базуки.
+ *
+ * \return Порожденный оружейный снаряд ручной базукой.
+ *
+ * Реализует создание оружейного снаряда ручной базукой.
+ */
 std::unique_ptr<Gunshell> HandBazooka::createGunshell(qreal x, qreal y)
 {    
     auto gunshell = std::make_unique<Gunshell>(scene(), 2);
