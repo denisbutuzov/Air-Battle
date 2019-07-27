8e7876e75aaab178732f5ebedbfcce682b9ff858
Denis Butuzov <qwaswedo@gmail.com>
2019-07-27 20:58:35 +0700

    Added documentation for functions in additionals files

--- src/additionals.cpp
+++ src/additionals.cpp
@@ -1,6 +1,11 @@
 #include <QDesktopWidget>
 #include <QApplication>
 
+/*!
+ * \param wgt Виджет, положение которого требуется выравнить.
+ *
+ * Реализует выравнивание переданного в качестве аргумента виджета посередине экрана.
+ */
 void move_to_center(QWidget *wgt)
 {
     QRect rect = wgt->geometry();
--- src/additionals.h
+++ src/additionals.h
@@ -4,6 +4,21 @@
 
 class QWidget;
 
+/*!
+ * \file
+ * \brief Файл вспомогательных функций.
+ *
+ * Файл включает в себя вспомогательные функции, используемые в коде игры.
+ */
+
+/*!
+ * \brief Шаблон функции для динамического приведения указателя с правами исключительного
+ * владения к другому.
+ *
+ * Реализует динамическое приведения указателя `std::unique_ptr<FromClass>` к указателю
+ * `std::unique_ptr<ToClass>`. Если динамическое приведение невозможно, функция возвращает
+ * указатель `std::unique_ptr<ToClass>`, инициализированный пустым указателем `nullptr`.
+ */
 template<typename ToClass, typename FromClass>
 std::unique_ptr<ToClass> dynamic_unique_cast(std::unique_ptr<FromClass> &&from)
 {
@@ -16,4 +31,5 @@ std::unique_ptr<ToClass> dynamic_unique_cast(std::unique_ptr<FromClass> &&from)
     return std::unique_ptr<ToClass>(nullptr);
 }
 
+///Функция для выравнивания виджета `QWidget` посередине экрана.
 void move_to_center(QWidget *wgt);
