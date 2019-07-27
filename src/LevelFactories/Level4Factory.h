a82700be9d322d14d5e40cd00619143c251da5e1
Denis Butuzov <qwaswedo@gmail.com>
2019-07-27 17:28:34 +0700

    Added documentation for Level4Factory-class

--- src/LevelFactories/Level4Factory.h
+++ src/LevelFactories/Level4Factory.h
@@ -2,12 +2,22 @@
 
 #include "AbstractLevelFactory.h"
 
+/*!
+ * \ingroup Level_factories
+ * \brief Фабричный класс четвертого уровня.
+ *
+ * Порождает игровые объекты для четвертого уровня игры.
+ */
 class Level4Factory
         : public AbstractLevelFactory
 {
 public:
+    ///Конструктор с одним аргументом.
     Level4Factory(std::weak_ptr<QGraphicsScene> scene);
+    ///Деструктор по умолчанию.
     virtual ~Level4Factory() override = default;
+    ///Метод, возвращающий объект врага.
     virtual std::unique_ptr<Enemy> enemy() override final;
+    ///Метод, возвращающий объект оружия.
     virtual std::unique_ptr<Weapon> weapon() override final;
 };
